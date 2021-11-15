5d3cff8ee502071aff3895de70969ef0d36730d7
mthaam <79216305+mthaam@users.noreply.github.com>
2021-11-15 20:39:38 +0100

    Currency UI ok
    Subpart "Currency Exchange" user interface has been built.

---
+++ Baluchon/Model/CurrenciesList.swift
@@ -0,0 +1,10 @@
+//
+//  CurrenciesList.swift
+//  Baluchon
+//
+//  Created by JEAN SEBASTIEN BRUNET on 15/11/21.
+//
+
+import Foundation
+
+let currencies = ["EUR", "US$", "CA$", "GPB", "YEN"]
--- Baluchon/ViewControllers/CurrencyExchangeViewController.swift
+++ Baluchon/ViewControllers/CurrencyExchangeViewController.swift
@@ -9,8 +9,61 @@ import UIKit
 
 class CurrencyExchangeViewController: UIViewController {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var containingViewPicker: UIView!
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var outputTextView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        makeRoundCornersToViews()
     }
 
+    @IBAction func convert(_ sender: Any) {
+        performConverting()
+    }
+}
+
+extension CurrencyExchangeViewController {
+
+    private func performConverting() {
+    }
+}
+
+extension CurrencyExchangeViewController {
+    private func makeRoundCornersToViews() {
+        inputTextView.layer.cornerRadius = 10
+        outputTextView.layer.cornerRadius = 10
+        containingViewPicker.layer.cornerRadius = 10
+    }
+}
+
+extension CurrencyExchangeViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencies.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return currencies[row]
+        } else {
+            return currencies[row]
+        }
+        return nil
+    }
+
+}
+
+extension CurrencyExchangeViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+                    inputTextView.resignFirstResponder()
+                    return false
+                }
+        return true
+    }
 }
